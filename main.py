a57199fae27f734e020dc754b6d2977c76851fd2
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2022-12-12 18:30:46 +0300

    obstacle logic, add score, lives, crow sounds

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame as pg
-
+pg.init()
 WIDTH, HEIGHT = 800, 600
 FPS = 60
-
+bg_music = pg.mixer.Sound('sounds/bgmus.mp3')
+bg_music.set_volume(0.5)
+bg_music.play(loops=-1)
 
 window = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
+
+font_score = pg.font.Font(None, 40)
+font_game_over = pg.font.Font(None, 40)
+
 img_bg = pg.image.load('image/bg/c.jpg')
 img_crow = pg.image.load('image/crow/crowanim.png')
 img_toprock = pg.image.load('image/rock/1.png')
@@ -20,6 +26,9 @@ timer = 10
 rocks = []
 bgs = []
 bgs.append(pg.Rect(0, 0, 288, 600))
+lives = 3
+scores = 0
+
 
 
 play = True
@@ -61,8 +70,10 @@ while play:
     # state parameters
     if state == 'start':
         if click and timer == 0 and len(rocks) == 0:
+            mah_sound = pg.mixer.Sound('sounds/mah.mp3')
+            mah_sound.play()
             state = 'play'
-        # возвращется на место
+        # возврат на место
         py += (HEIGHT // 2 - py) * 0.1
         player.y = py
     elif state == 'play':
@@ -75,12 +86,12 @@ while play:
         sy = (sy + ay + 1) * 0.98
         player.y = py
 
-        # rocks если труба перешла границу экрана
+        # rocks
         if len(rocks) == 0 or rocks[len(rocks)-1].x < WIDTH - 200:
             rocks.append(pg.Rect(WIDTH, 0, 62, 190))
             rocks.append(pg.Rect(WIDTH, 420, 50, 190))
 
-        # столкновение
+        # collision
         if player.top < 0 or player.bottom > HEIGHT:
             state = 'fail'
 
@@ -89,20 +100,32 @@ while play:
                 state = 'fail'
 
     elif state == 'fail':
-        # сброс на начало
+        # reset to start
         sy, ay = 0, 0
-        state = 'start'
-        timer = 60
+        # sound
+        fail_sound = pg.mixer.Sound('sounds/carrr.mp3')
+        fail_sound.play()
+        lives -= 1
+        if lives > 0:
+            state = 'start'
+            # delay before start
+            timer = 60
+        else:
+            state = 'Game over'
+            timer = 120
     else:
-        pass
+        py += sy
+        sy = (sy + ay + 1) * 0.98
+        player.y = py
 
-    # draw
-    window.fill(pg.Color('black'))
-    #
+        if timer == 0:
+            play = False
+
+    # bg
     for bg in bgs:
         window.blit(img_bg, bg)
-    #
 
+    # obstacle location
     for rock in rocks:
         if rock.y == 0:
             rect = img_botrock.get_rect(bottomleft=rock.bottomleft)
@@ -112,9 +135,16 @@ while play:
             window.blit(img_toprock, rect)
 
     image = img_crow.subsurface(60 * int(frame), 0, 37, 40)
-    # задирать нос
+    # lift up head anim
     image = pg.transform.rotate(image, -sy * 2)
     window.blit(image, player)
+    # scores
+    text = font_score.render('Score: ' + str(scores), 1, pg.Color('grey'))
+    window.blit(text, (10, 10))
+    # lives
+    text = font_score.render('Lives: ' + str(lives), 1, pg.Color('grey'))
+    window.blit(text, (10, HEIGHT - 30))
+
     pg.display.update()
     clock.tick(FPS)
 
