ff1d16e2c60c4e64fdca1d45fcf0de1f38b72ab6
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2022-12-13 14:28:57 +0300

    correct game, bug fix, gate.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame as pg
+from random import randint
+
 pg.init()
 WIDTH, HEIGHT = 800, 600
 FPS = 60
-bg_music = pg.mixer.Sound('sounds/bgmus.mp3')
-bg_music.set_volume(0.5)
+
+bg_music = pg.mixer.Sound('sounds/bgmus.wav')
+bg_music.set_volume(0.3)
 bg_music.play(loops=-1)
+mah_sound = pg.mixer.Sound('sounds/mah.wav')
+mah_sound.set_volume(0.5)
+fail_sound = pg.mixer.Sound('sounds/carrr.wav')
+fail_sound.set_volume(0.5)
 
 window = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
+
+pg.display.set_caption('Flappy crow')
+pg.display.set_icon(pg.image.load('image/crow/raven.ico'))
 
 font_score = pg.font.Font(None, 40)
 font_game_over = pg.font.Font(None, 40)
@@ -25,55 +35,59 @@ frame = 0
 timer = 10
 rocks = []
 bgs = []
+rock_scores = []
 bgs.append(pg.Rect(0, 0, 288, 600))
 lives = 3
 scores = 0
-
-
+rock_speed = 3
+# rock gate
+gate_size = 200
+gate_pos = HEIGHT // 2
 
 play = True
 while play:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             play = False
-
     # controls
     press = pg.mouse.get_pressed()
     keys = pg.key.get_pressed()
     click = press[0] or keys[pg.K_SPACE]
 
-    # timer если значение > 0 то должен стремиться к 0
-    if timer > 0:
+    # timer
+    if timer:
         timer -= 1
-    # смена кадров
+
+    # change frame
     frame = (frame + 0.1) % 4
 
     for i in range(len(bgs)-1, -1, -1):
         bg = bgs[i]
-        bg.x -= 1
+        bg.x -= rock_speed // 2
         # drop rocks if off window
         if bg.right < 0:
             bgs.remove(bg)
 
-        # склеивание картинок для фона
+        # gluing picture for the background
         if bgs[len(bgs)-1].right <= WIDTH:
             bgs.append(pg.Rect(bgs[len(bgs)-1].right, 0, 288, 600))
 
     # obstacles
     for i in range(len(rocks)-1, -1, -1):
         rock = rocks[i]
-        rock.x -= 4
+        rock.x -= rock_speed
         # drop rocks if off window
         if rock.right < 0:
             rocks.remove(rock)
+            if rock in rock_scores:
+                rock_scores.remove(rock)
 
     # state parameters
     if state == 'start':
-        if click and timer == 0 and len(rocks) == 0:
-            mah_sound = pg.mixer.Sound('sounds/mah.mp3')
+        if click and not timer and not len(rocks):
             mah_sound.play()
             state = 'play'
-        # возврат на место
+        # return pos
         py += (HEIGHT // 2 - py) * 0.1
         player.y = py
     elif state == 'play':
@@ -81,15 +95,21 @@ while play:
             ay = -2
         else:
             ay = 0
-        # gravitation player
+        # player gravity
         py += sy
         sy = (sy + ay + 1) * 0.98
         player.y = py
 
-        # rocks
-        if len(rocks) == 0 or rocks[len(rocks)-1].x < WIDTH - 200:
-            rocks.append(pg.Rect(WIDTH, 0, 62, 190))
-            rocks.append(pg.Rect(WIDTH, 420, 50, 190))
+        # add rocks, gate
+        if not len(rocks) or rocks[len(rocks)-1].x < WIDTH - 200:
+            rocks.append(pg.Rect(WIDTH, 0, 62, gate_pos - gate_size // 2))
+            rocks.append(pg.Rect(WIDTH, gate_pos + gate_size // 2, 52, HEIGHT - gate_pos + gate_size // 2))
+            # change pos gate algorithm
+            gate_pos += randint(-100, 100)
+            if gate_pos < gate_size:
+                gate_pos = gate_size
+            elif gate_pos > HEIGHT - gate_size:
+                gate_pos = HEIGHT - gate_size
 
         # collision
         if player.top < 0 or player.bottom > HEIGHT:
@@ -98,27 +118,34 @@ while play:
         for rock in rocks:
             if player.colliderect(rock):
                 state = 'fail'
+            # add scores after rock
+            if rock.right < player.left and rock not in rock_scores:
+                rock_scores.append(rock)
+                scores += 5
+                # score to speed
+                rock_speed = 3 + scores // 100
 
     elif state == 'fail':
+        fail_sound.play()
         # reset to start
         sy, ay = 0, 0
-        # sound
-        fail_sound = pg.mixer.Sound('sounds/carrr.mp3')
-        fail_sound.play()
+        # return gate pos
+        gate_pos = HEIGHT // 2
         lives -= 1
-        if lives > 0:
+        if lives:
             state = 'start'
-            # delay before start
+            # delay before start 1 min
             timer = 60
         else:
-            state = 'Game over'
+            font_score = pg.font.Font(None, 60)
+            state = 'game over'
             timer = 120
     else:
         py += sy
         sy = (sy + ay + 1) * 0.98
         player.y = py
-
-        if timer == 0:
+        # if timer == 0
+        if not timer:
             play = False
 
     # bg
@@ -127,21 +154,21 @@ while play:
 
     # obstacle location
     for rock in rocks:
-        if rock.y == 0:
+        if not rock.y:
             rect = img_botrock.get_rect(bottomleft=rock.bottomleft)
             window.blit(img_botrock, rect)
         else:
             rect = img_botrock.get_rect(topleft=rock.topleft)
             window.blit(img_toprock, rect)
 
-    image = img_crow.subsurface(60 * int(frame), 0, 37, 40)
-    # lift up head anim
+    image = img_crow.subsurface(60 * int(frame), 0, 40, 50)
+    # raven spin anim 'nose up'
     image = pg.transform.rotate(image, -sy * 2)
     window.blit(image, player)
-    # scores
-    text = font_score.render('Score: ' + str(scores), 1, pg.Color('grey'))
+    # scores text
+    text = font_score.render('Score: ' + str(scores), 11, pg.Color('grey'))
     window.blit(text, (10, 10))
-    # lives
+    # lives text
     text = font_score.render('Lives: ' + str(lives), 1, pg.Color('grey'))
     window.blit(text, (10, HEIGHT - 30))
 
